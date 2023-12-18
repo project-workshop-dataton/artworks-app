7035ac4631e4d5e79494f1588abcb4fdf97d7bba
Andrew Efimov <68296704+Deopster@users.noreply.github.com>
2023-12-18 21:31:46 +0300

    Update Logic_init.py

--- Logic_init.py
+++ Logic_init.py
@@ -12,7 +12,7 @@ if not os.path.exists(path_to_word_vectors):
     gdown.download(url, path_to_word_vectors, quiet=False)
 if not os.path.exists(path_to_model):
     url = 'https://drive.google.com/uc?id=1cWYd9jYG9nGU0jDJDB3G6X6s-GyB2v_6'
-    gdown.download(url, path_to_word_vectors, quiet=False)
+    gdown.download(url, path_to_model, quiet=False)
 df = pd.read_csv('./data/arts_data_model_and_front.csv')
 model = KeyedVectors.load_word2vec_format(path_to_word_vectors, binary=True, )
 
