e99801f723720b8af9ff0fc95332804a0ae4b30f
Andrew Efimov <68296704+Deopster@users.noreply.github.com>
2023-12-18 18:13:38 +0300

    load_model

--- Logic_init.py
+++ Logic_init.py
@@ -3,11 +3,12 @@ from gensim.models import KeyedVectors
 import platform, os
 import pandas as pd
 # load = ImageLoader() #создание объекта load
-if platform.system() == "Windows":
-    path_to_word_vectors = os.environ['vector']
-else:
-    path_to_word_vectors = '~/Desktop/Projects/Data/GoogleNews/GoogleNews-vectors-negative300.bin.gz'
+import gdown
+import os
+path_to_word_vectors ="./data/GoogleNews-vectors-negative300.bin.gz"
+if not os.path.exists(path_to_word_vectors):
+    url = 'https://drive.google.com/uc?id=1TAkVgZNWVLvuEiUsEwn_oSbVLFrRfRVG'
+    gdown.download(url, path_to_word_vectors, quiet=False)
 model = KeyedVectors.load_word2vec_format(path_to_word_vectors, binary=True, )
-
 df = pd.read_csv('~/Desktop/Projects/Data/arts_data_model_and_front.csv')
 arts_descriptions = df['feature_joined_text'].to_list()
