8fa3d70404cbd19457e09ea6a526cbb573b89247
Evgeniy <eugene.ovechkin@gmail.com>
2023-12-20 17:22:44 +0300

    add dummy image

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from typing import Union
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from Logic_init import model,load, df, arts_descriptions
-#from Logic_init import model, df, arts_descriptions
+from Logic_init import model, load, df, arts_descriptions
+# from Logic_init import model, df, arts_descriptions
 import model_implementing.word2vec as w2v
 
 # TODO: перепутана ширина с высотой
@@ -16,8 +16,8 @@ import model_implementing.word2vec as w2v
 # TODO: подумать что можно сделать со скоростью ответа - сделать параллельными запросы картинок
 
 
-
 app = FastAPI()
+
 
 @app.get("/api")
 def search(q: Union[str, None] = None):
@@ -31,15 +31,14 @@ def search(q: Union[str, None] = None):
     '''
 
     # most_similar_descriptions = w2v.find_most_similar_text(model= model, query='impressionism',texts = ['man in black', 'starry night'], top_k = 1)
-   
 
-    most_similar_descriptions = w2v.find_most_similar_text(model= model, query=q,texts = arts_descriptions, top_k = 3)
+    most_similar_descriptions = w2v.find_most_similar_text(
+        model=model, query=q, texts=arts_descriptions, top_k=3)
     print(most_similar_descriptions)
-    most_similar_arts = df[df['feature_joined_text'].isin(most_similar_descriptions)]
+    most_similar_arts = df[df['feature_joined_text'].isin(
+        most_similar_descriptions)]
 
     # df[df['feature_joined_text'] in most_similar_descriptions]
-
-    
 
     # получение ИД
     # из файла достать 3 объекта
@@ -47,14 +46,18 @@ def search(q: Union[str, None] = None):
     # вернуть в согласованном формате на фронт
 
     # return 3 suggestions
-    most_similar_arts['release_date'] = most_similar_arts['release_date'].astype(str)
-    data = most_similar_arts[['artwork_id','title', 'name', 'medium', 'classification', 'release_date']].to_dict('records')
-    final=[]
+    most_similar_arts['release_date'] = most_similar_arts['release_date'].astype(
+        str)
+    data = most_similar_arts[['artwork_id', 'title', 'name',
+                              'medium', 'classification', 'release_date']].to_dict('records')
+    final = []
     for object in data:
-        load.newrun(object['title'],object['artwork_id'])  # строка поиска картинки по сути название картины + автор
-        #object['link'] = './images/'+str(object['artwork_id'])+'.png'
-        object['link'] = './images/' + '6712' + '.png'
+        # строка поиска картинки по сути название картины + автор
+        # load.newrun(object['title'], object['artwork_id'])
+        # object['link'] = './images/'+str(object['artwork_id'])+'.png'
+        object['link'] = './images/' + 'dummy' + '.png'
         final.append(object)
+
     # dummy = {
     #     'id': '29296',
     #     'title': 'Picasso Dessins',
