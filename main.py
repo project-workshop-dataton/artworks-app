d0fc052d1526f2d25eb44b64d6392396c009ce72
Evgeniy <eugene.ovechkin@gmail.com>
2023-12-20 18:27:05 +0300

    remove some trash

--- main.py
+++ main.py
@@ -5,16 +5,10 @@ from Logic_init import model, load, df, arts_descriptions
 # from Logic_init import model, df, arts_descriptions
 import model_implementing.word2vec as w2v
 
-# TODO: перепутана ширина с высотой
-# TODO: картинку наложить на небелый фон
-# TODO: предобработать год, чтоб нормальны выводился
-# TODO: добавить две кнопки и метод обработки лайк-дизлайк
-# TODO: подобрать шрифты, чтобы нормально выглядело название
-# TODO: сделать обрезание текстов по количеству строк или символов
-# TODO: зафиксировать разметку, чтобы не зависило от большого текста
-# TODO: смержить с веткой development
-# TODO: подумать что можно сделать со скоростью ответа - сделать параллельными запросы картинок
+from pprint import pprint
 
+# TODO: предобработать признаки - title, name, medium, classification, year
+# TODO: добавить две кнопки и метод обработки лайк-дизлайк
 
 app = FastAPI()
 
@@ -28,6 +22,7 @@ def search(q: Union[str, None] = None):
     - description
     - type
     - date
+    - link
     '''
 
     # most_similar_descriptions = w2v.find_most_similar_text(model= model, query='impressionism',texts = ['man in black', 'starry night'], top_k = 1)
@@ -40,16 +35,14 @@ def search(q: Union[str, None] = None):
 
     # df[df['feature_joined_text'] in most_similar_descriptions]
 
-    # получение ИД
-    # из файла достать 3 объекта
-    # достать картинку из поисковика
-    # вернуть в согласованном формате на фронт
-
     # return 3 suggestions
     most_similar_arts['release_date'] = most_similar_arts['release_date'].astype(
         str)
     data = most_similar_arts[['artwork_id', 'title', 'name',
                               'medium', 'classification', 'release_date']].to_dict('records')
+
+    pprint(data)
+
     final = []
     for object in data:
         # строка поиска картинки по сути название картины + автор
@@ -67,14 +60,10 @@ def search(q: Union[str, None] = None):
     #     'date': '1926',
     #     'link': 'images/29296.jpg'
     # }
-
     # data = [dummy, dummy, dummy]
-    print()
-    print()
-    print()
+
     print()
     print(data)
-    # data = []
     return data
 
 
