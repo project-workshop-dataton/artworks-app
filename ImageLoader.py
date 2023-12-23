56cdfbb9dae8ce55409d6deb6c048449b2ac9018
Evgeniy <eugene.ovechkin@gmail.com>
2023-12-23 16:43:59 +0300

    add readme

--- ImageLoader.py
+++ ImageLoader.py
@@ -6,63 +6,76 @@ import imghdr
 from PIL import Image, ImageOps, ImageFont
 from urllib.parse import quote
 from PIL import ImageDraw
+
+
 def photoshop(filename):
+    """обработчик картинок
+    """
+
     img = Image.open(filename)
     rgba = img.convert("RGBA")
-    resize = ImageOps.pad(rgba, (300,200), color=(255, 255, 255, 0))
-    #border = (0, 0, 0, 0)
-    #final = ImageOps.expand(resize, border=border, fill='black') #задел на дополнение картинки, вместо обрезки
-    resize.save(filename,"PNG")
+    resize = ImageOps.pad(rgba, (300, 200), color=(255, 255, 255, 0))
+    # border = (0, 0, 0, 0)
+    # final = ImageOps.expand(resize, border=border, fill='black') #задел на дополнение картинки, вместо обрезки
+    resize.save(filename, "PNG")
+
+
+# класс загрузчика картинок
 class ImageLoader:
     def __init__(self):
-        self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' 
-      'AppleWebKit/537.11 (KHTML, like Gecko)'
-      'Chrome/23.0.1271.64 Safari/537.11',
-      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-      'Accept-Charset': 'ISO-8859-1,utf-8;q=0.3,*;q=0.7',
-      'Accept-Encoding': 'none',
-      'Accept-Language': 'ru,en;q=0.9',
-      'Connection': 'keep-alive'}
+        self.headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
+                        'AppleWebKit/537.11 (KHTML, like Gecko)'
+                        'Chrome/23.0.1271.64 Safari/537.11',
+                        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.3,*;q=0.7',
+                        'Accept-Encoding': 'none',
+                        'Accept-Language': 'ru,en;q=0.9',
+                        'Connection': 'keep-alive'}
         self.work_path = "./static/images/"
-    def newrun(self,req,id):
-        self.query = quote(req.replace(' ', '+').replace('…', '').replace('@', '').replace('(', '').replace(')', '').replace("‘", " "))
+
+    def newrun(self, req, id):
+        self.query = quote(req.replace(' ', '+').replace('…', '').replace('@',
+                           '').replace('(', '').replace(')', '').replace("‘", " "))
         request_url = 'https://www.moma.org/collection/works/' + str(id)
-        #request = urllib.request.Request(request_url, 2, headers=self.headers)
-        #response = urllib.request.urlopen(request)
-        #html = response.read().decode('utf8')
-        #link = re.findall('murl&quot;:&quot;(.*?)&quot;', html)
-        #print(link)
-    def run(self,req,id):
-        self.query = quote(req.replace(' ', '+').replace('…', '').replace('@', '').replace('(', '').replace(')', '').replace("‘"," "))
+        # request = urllib.request.Request(request_url, 2, headers=self.headers)
+        # response = urllib.request.urlopen(request)
+        # html = response.read().decode('utf8')
+        # link = re.findall('murl&quot;:&quot;(.*?)&quot;', html)
+        # print(link)
+
+    def run(self, req, id):
+        self.query = quote(req.replace(' ', '+').replace('…', '').replace('@',
+                           '').replace('(', '').replace(')', '').replace("‘", " "))
         request_url = 'https://www.bing.com/images/async?q=' + self.query \
                       + '&qft=' + 'filterui:photo-photo'+'+filterui:aspect-tall'
-        request = urllib.request.Request(request_url, None, headers=self.headers)
+        request = urllib.request.Request(
+            request_url, None, headers=self.headers)
         response = urllib.request.urlopen(request)
         html = response.read().decode('utf8')
         link = re.findall('murl&quot;:&quot;(.*?)&quot;', html)
-        #print(link[0::5])
-        temp_load=0
+        # print(link[0::5])
+        temp_load = 0
         while True:
-            if self.save_image(link[temp_load],id):
-                    break
-            temp_load+=1
-        #print("Скачивание Выполнено за {} итерации".format(temp_load+1))
+            if self.save_image(link[temp_load], id):
+                break
+            temp_load += 1
+        # print("Скачивание Выполнено за {} итерации".format(temp_load+1))
 
-
-    def save_image(self, link,id):
+    def save_image(self, link, id):
         try:
             if " " in link:
                 return False
             request = urllib.request.Request(link, None, self.headers)
             image = urllib.request.urlopen(request, timeout=2).read()
             if not imghdr.what(None, image):
-                #print('Битая картинка т.к не удаётся определить расширение {}\n'.format(link))
-                raise ValueError('Битая картинка т.к не удаётся определить расширение {}\n'.format(link))
+                # print('Битая картинка т.к не удаётся определить расширение {}\n'.format(link))
+                raise ValueError(
+                    'Битая картинка т.к не удаётся определить расширение {}\n'.format(link))
             filename = self.work_path+str(id)+".png"
             with open(filename, 'wb') as f:
                 f.write(image)
             photoshop(filename)
             return True
         except Exception as e:
-            #print('403 '+link, e)
+            # print('403 '+link, e)
             return False
--- main.py
+++ main.py
@@ -2,31 +2,30 @@ from typing import Union
 from pydantic import BaseModel
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
-# from Logic_init import model, load, df, arts_descriptions
 from Logic_init import model, df, arts_descriptions
 import model_implementing.word2vec as w2v
 from pprint import pprint
 
+# создание приложения
 app = FastAPI()
 
 
+# обработчик поискового запроса
 @app.get("/api")
 async def search(q: Union[str, None] = None):
     '''
     Формат сообщения - массив объектов:
-    - artwork_id
-    - pub_title
-    - name
-    - medium
-    - classification
-    - pub_year
-    - link
+    - artwork_id - ид объекта в базе
+    - pub_title - название
+    - name - имя автора
+    - medium - описание объекта
+    - classification - тип прозведения исскуства
+    - pub_year - год создания
+    - link - ссылка на иллюстрация
     '''
 
     most_similar_descriptions = w2v.find_most_similar_text(
-        model=model, query=q, texts=arts_descriptions, top_k=3)
-
-    # print(most_similar_descriptions)
+        model=model, query=q, texts=arts_descriptions, top_k=3)  # type: ignore
 
     most_similar_arts = df[df['feature_joined_text'].isin(
         most_similar_descriptions)]
@@ -44,22 +43,25 @@ async def search(q: Union[str, None] = None):
         # object['link'] = './images/'+str(object['artwork_id'])+'.png'
         # final.append(object)
 
-    dummy = {'artwork_id': '46333',
-             'classification': 'Photograph',
-             'link': './images/dummy.png',
-             'medium': 'Chromogenic color print. Chromogenic color print. Chromogenic color print. Chromogenic color print.Chromogenic color print.',
-             'name': 'Stuart Klipper',
-             'pub_title': 'FOG AND ICE BLINK',
-             'pub_year': '1981'}
-
+    # Объект заглушка для мок-тестирования
+    # dummy = {'artwork_id': '46333',
+    #          'classification': 'Photograph',
+    #          'link': './images/dummy.png',
+    #          'medium': 'Chromogenic color print. Chromogenic color print. Chromogenic color print. Chromogenic color print.Chromogenic color print.',
+    #          'name': 'Stuart Klipper',
+    #          'pub_title': 'FOG AND ICE BLINK',
+    #          'pub_year': '1981'}
     # pprint(data)
+
     return data
 
 
+# сбор обратной связи по качеству ответа
 @app.post("/api/feedback")
 async def feedback(request: Request):
     fb = await request.json()
     print(fb)
 
 
+# подключаем статические файла сайта, включая веб-приложение
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
